efc0f74fc8ae909b20d56994c3255de6ca163179
EasonLee2006 <eason2006eason@gmail.com>
2026-01-23 05:39:21 +0800

    modified main.py: added /book to api

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException
 from utils.database import get_db_connection
+from pydantic import BaseModel
+from enum import Enum
+
+class SlotStatus(Enum):
+    AVAILABLE = 0
+    BOOKED = 2
+
+class BookingRequest(BaseModel):
+    slot_id: int
+    user_id: int
 
 # Initialize the app
 app = FastAPI(
@@ -30,3 +40,54 @@ def get_stalls( conn = Depends(get_db_connection) ):
         return stalls
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching stalls: {e}")
+    
+@app.post("/book")
+def book_stall( request: BookingRequest, conn = Depends(get_db_connection) ):
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            "SELECT status FROM availability WHERE slot_id = %s FOR UPDATE;", 
+            (request.slot_id,)
+        )
+        row = cursor.fetchone()
+        if not row:
+            raise HTTPException(status_code=404, detail="Slot not found")
+        
+        current_status = row['status']
+
+        if current_status != SlotStatus.AVAILABLE.value:
+            conn.rollback() # Cancel transaction
+            # Return 409 Conflict (standard for "state conflict")
+            raise HTTPException(status_code=409, detail="Too slow! This slot is already booked.")
+        
+        cursor.execute(
+            "UPDATE availability SET status = %s WHERE slot_id = %s;",
+            (SlotStatus.BOOKED.value, request.slot_id)
+        )
+        cursor.execute(
+            """
+            INSERT INTO bookings (slot_id, user_id, payment_status) 
+            VALUES (%s, %s, 'PENDING') 
+            RETURNING booking_id;
+            """,
+            (request.slot_id, request.user_id)
+        )
+        new_booking_id = cursor.fetchone()['booking_id']
+
+        conn.commit() # Commit transaction
+        
+        return {
+            "status": "success", 
+            "message": "Booking confirmed!", 
+            "booking_id": new_booking_id
+        }
+    except Exception as e:
+        conn.rollback() # If any error happens, undo everything
+        # If it's already an HTTPException (like 409 or 404), re-raise it
+        if isinstance(e, HTTPException):
+            raise e
+        # Otherwise, it's a server error
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    finally:
+        cursor.close()
